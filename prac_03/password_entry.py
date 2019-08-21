19b5dbf75fa90e2040256cc47be837c2017a6ff8
MartinArmstrongAU <martin.armstrong@my.jcu.edu.au>
2019-08-21 13:27:33 +1000

    Refactor password program to use functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,10 +1,23 @@
 """ Martin Armstrong """
 
-min_length = 8
-password = input("Please enter a password: ")
-while len(password) < min_length:
-    print("That password is less than 8 characters. Please try again.")
-    password = input("Please enter a password: ")
 
-for i in range(len(password)):
-    print("*", end="")
+def main():
+    min_length = 8
+    password = get_password(min_length)
+    print_hidden_password(password)
+
+
+def get_password(min_length):
+    password = input("Please enter a password: ")
+    while len(password) < min_length:
+        print("That password is less than 8 characters. Please try again.")
+        password = input("Please enter a password: ")
+    return password
+
+
+def print_hidden_password(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+
+main()
