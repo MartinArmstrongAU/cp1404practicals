8fd3d655a918c957d69eec8d192f92cbc3dff3f8
MartinArmstrongAU <martin.armstrong@my.jcu.edu.au>
2019-08-21 13:34:08 +1000

    Update code to be more efficient

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -2,22 +2,21 @@
 
 
 def main():
-    min_length = 8
+    min_length = 6
     password = get_password(min_length)
     print_hidden_password(password)
 
 
 def get_password(min_length):
-    password = input("Please enter a password: ")
+    password = input("Please enter a password that is at least {} characters: ".format(min_length))
     while len(password) < min_length:
-        print("That password is less than 8 characters. Please try again.")
-        password = input("Please enter a password: ")
+        print("That password is less than {} characters. Please try again.".format(min_length))
+        password = input("Please enter a password that is at least {} characters: ".format(min_length))
     return password
 
 
 def print_hidden_password(password):
-    for i in range(len(password)):
-        print("*", end="")
+    print("*" * len(password))
 
 
 main()
