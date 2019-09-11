0f3f0ce46b2b1720cb8faade79e3c082c861894e
MartinArmstrongAU <martin.armstrong@my.jcu.edu.au>
2019-09-11 12:56:41 +1000

    Refactor function naming to be conventional

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,12 +8,12 @@ NUMBERS_PER_LINE = 6
 def main():
     number_of_quick_picks = int(input("How many quick picks? "))
     for i in range(number_of_quick_picks):
-        row_numbers_generated = get_row_numbers()
+        row_numbers_generated = calculate_row_numbers()
         row_numbers_generated.sort()
         print(" ".join("{:2}".format(number) for number in row_numbers_generated))
 
 
-def get_row_numbers():
+def calculate_row_numbers():
     numbers = []
     for i in range(NUMBERS_PER_LINE):
         number = random.randint(MINIMUM, MAXIMUM)
