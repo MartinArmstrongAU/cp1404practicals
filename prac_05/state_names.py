022e6a1c759740c9e8e61bba972e77abebfb9e0e
MartinArmstrongAU <martin.armstrong@my.jcu.edu.au>
2019-09-11 12:50:36 +1000

    Reformat print statement to use the .format function

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -14,7 +14,7 @@ for state in STATE_NAMES:
 state = input("Enter short state: ").upper()
 while state != "":
     if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
+        print("{} is {}".format(state, STATE_NAMES[state]))
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
