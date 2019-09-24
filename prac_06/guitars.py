e4c27bcc3f1453fc5c339aefbc801ed9da0a6d75
MartinArmstrongAU <martin.armstrong@my.jcu.edu.au>
2019-09-25 01:54:08 +1000

    add functionality to print all guitar details

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,14 +5,30 @@ def main():
     guitars = []
 
     print("My Guitars!")
-    name = input("Names: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input(("Cost ($): ")))
-        guitar_to_add = Guitar(name, year, cost)
-        guitars.append(guitar_to_add)
-        print("{} added".format(guitar_to_add))
-        name = input("Names: ")
+    # name = input("Names: ")
+    # while name != "":
+    #     year = int(input("Year: "))
+    #     cost = float(input("Cost ($): "))
+    #     guitar_to_add = Guitar(name, year, cost)
+    #     guitars.append(guitar_to_add)
+    #     print("{} added.".format(guitar_to_add))
+    #     name = input("Names: ")
+
+    guitars.append(Guitar("Fender Stratocaster", 2014, 765.4))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        guitars.sort()
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                                      vintage_string))
+    else:
+        print("No guitars :( Quick, go and buy one!")
 
 
 if __name__ == '__main__':
