297c7dea4c8ac727279e06ee5c1e16f9ddcb96d7
MartinArmstrongAU <martin.armstrong@my.jcu.edu.au>
2019-09-24 23:44:56 +1000

    add __str__ method to the class

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -7,5 +7,7 @@ def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
+    print(ruby)
+
 
 main()
